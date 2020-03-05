bae43f101bebaeca46e5e3f4f548dda6d28ed54b
gg <labankiplagat81@gmail.com>
2020-03-05 08:24:37 +0300

    product templates

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
@@ -12,6 +12,11 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=self.slug)
+
+    
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
@@ -31,6 +36,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
 
     
         
