9b27d052a0d8f15c1101336e339f6958b2eee8fc
gg <labankiplagat81@gmail.com>
2020-03-05 14:53:56 +0300

    Product image detail view.

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -14,6 +14,6 @@ def product_list(request, category_slug=None):
 
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(id=id, slug=slug, available=True)
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
 
     return render(request, 'product_detail.html', {'product': product})
