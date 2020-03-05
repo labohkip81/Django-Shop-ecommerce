b5dd9795eadcc2166f42e4c499494600ddbe498d
gg <labankiplagat81@gmail.com>
2020-03-05 12:01:19 +0300

    Product list addition.

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -6,7 +6,7 @@ from .models import Category, Product
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Products.objects.filter(available=True)
+    products = Product.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
